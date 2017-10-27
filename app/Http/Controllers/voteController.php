32e7d2eff7263393e70ad751c7e713e58067c93b
LuisVuitton <holbikl@gmail.com>
2017-10-27 12:24:38 +0200

    added vote.blade

---
+++ app/Http/Controllers/voteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class voteController extends Controller
+{
+    //
+
+    public function voting()
+    {
+        $view = view ('poll_app/vote')
+        $all_poll = 
+    }
+}
---
+++ resources/views/poll_app/vote.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+    <body>
+   
+    <form>
+    Please select your choice:
+       
+        <div>
+            <input type="radio" id="option1"
+            name="option" value="option1">
+            <label for="optionChoice1">option 1</label>
+        
+            <input type="radio" id="option2"
+            name="option" value="2">
+            <label for="optionChoice2">option 2</label>
+        
+            <input type="radio" id="option3"
+            name="option" value="option3">
+            <label for="optionChoice3">option 3</label>
+
+            <input type="radio" id="option4"
+            name="option" value="option4">
+            <label for="optionChoice3">option 4</label>
+
+            <input type="radio" id="option5"
+            name="option" value="option5">
+            <label for="optionChoice3">option 5</label>
+            </div>
+
+            <div>
+            <button type="submit">Submit</button>
+            </div>
+  </form>
+    
+    </body>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</html>
